29f4c7c6697d670882f4a4fa2fd3bad1d7d476cb
JMLG12 <113480389+JMLG12@users.noreply.github.com>
2022-12-22 20:25:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hola, me llamo consola papas y este es el proyecto de consola");
+var primer_nombre="Juan Manuel";
+var primer_apellido="Leon Guadarrama";
+var nombre_completo=primer_nombre.concat(primer_apellido);
+console.log(nombre_completo)
+var a="10";
+var b="5";
+var suma= a + b;
+console.log(suma);
+var resta= a - b;
+console.log(resta);
+var multiplicar= a * b;
+console.log(multiplicar);
+var division= a / b;
+console.log(division);
